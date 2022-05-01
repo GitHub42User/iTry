9949510a55784c461f9921791c2493d65322f9f7
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-05-01 15:42:00 +0200

    Update 1stONE.py

--- 1stONE.py
+++ 1stONE.py
@@ -1,1 +1,19 @@
-sytem.out.println(" Hello World ")
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Image Captioning')
+
+
+
+
+
+
+
+
+
+
+
+
+
+streamlit run 1stONE.py
